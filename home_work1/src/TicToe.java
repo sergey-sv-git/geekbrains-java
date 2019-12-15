d30b4528a2504d58eff42fb2ef6a1bbe2919fc65
sergey-sv-git <andreev-sv@bk.ru>
2019-12-15 22:51:28 +0300

    переработал алгоритм определения победы, разработал ИИ

--- home_work1/src/TicToe.java
+++ home_work1/src/TicToe.java
@@ -9,8 +9,6 @@ public class TicToe {
     private static final char DOT_EMPTY = '.';
     private static final Scanner SCANNER = new Scanner(System.in);
     private static final Random RANDOM = new Random();
-    private static int bestX;
-    private static int bestY;
 
     private static char[][] field;
     private static int fieldSizeX;
@@ -58,7 +56,7 @@ public class TicToe {
     }
 
     private static boolean isValidCell(int x, int y){
-        return x >= 0 && x <= fieldSizeX && y >= 0 && y <= fieldSizeY;
+        return x >= 0 && x < fieldSizeX && y >= 0 && y < fieldSizeY;
     }
 
     private static boolean isEmptyCell(int x, int y){
@@ -75,38 +73,43 @@ public class TicToe {
         field[y][x] = DOT_AI;
     }
     private static void aiTurn (int bestX, int bestY) {
-        if (isEmptyCell(bestX,bestY)&&isValidCell(bestX, bestY)) {
-            field[bestY][bestX] = DOT_AI;
-        }
+        if (isEmptyCell(bestX,bestY)&&isValidCell(bestX, bestY)){
+            field[bestY][bestX] = DOT_AI;}
         else aiTurn();
     }
 
     private static boolean checkWin (char c){
-        int Horizont, Vertical, Diag1, Diag2, Diag3, Diag4, Diag5, Diag6;
+        int tempVin;
         for (int y = 0; y < fieldSizeY; y++) {
-            Horizont = 0;
-            Vertical = 0;
-            Diag1 = 0;
-            Diag2 = 0;
-            Diag3 = 0;
-            Diag4 = 0;
-            Diag5 = 0;
-            Diag6 = 0;
             for (int x = 0; x < fieldSizeX; x++) {
-                if (field[y][x] == c) Horizont++;
-                if (field[x][y] == c) Vertical++;
-                if (field[x][x] == c) Diag1++;
-                if (field[x][fieldSizeX - 1 - x] == c) Diag2++;
-                if ((x+1) < fieldSizeX && field[x][x+1] == c) Diag3++;
-                if (fieldSizeX-2-x > 0 && field[x][fieldSizeX-2-x] == c) Diag4++;
-                if ((x+1) < fieldSizeY && field[x+1][x] == c) Diag5++;
-                if (fieldSizeY-2-x > 0 && field[fieldSizeY-2-x][x] == c) Diag6++;
-
-                if (((Horizont==vin && field[y][0]!=c)||(Horizont==vin && field[y][fieldSizeX-1]!=c))||(Horizont==5)) return true;
-                if (((Vertical==vin && field[fieldSizeY-1][y]!=c) || (Vertical==vin && field[0][y]!=c))||(Vertical==5)) return true;
-                if (((Diag1==vin && field[0][0]!=c)||(Diag1==vin && field[fieldSizeY-1][fieldSizeX-1]!=c))||(Diag1==5)) return true;
-                if (((Diag2==vin && field[0][fieldSizeX-1]!=c)||(Diag2==vin && field[fieldSizeY-1][0]!=c))||(Diag2==5)) return true;
-                if (Diag3==vin||Diag4==vin||Diag5==vin||Diag6==vin) return true;
+                if (isValidCell(x+3,y)){
+                    tempVin = 0;
+                    for (int i = 0; i < 4; i++) {
+                        if ((field[y][x+i]) == c) tempVin++;
+                        if (tempVin == vin) return true;
+                    }
+                }
+                if (isValidCell(y, x + 3)) {
+                    tempVin = 0;
+                    for (int i = 0; i < 4; i++) {
+                        if ((field[x+i][y] == c)) tempVin++;
+                        if (tempVin == vin) return true;
+                    }
+                }
+                if (isValidCell(x+3, y+3)){
+                    tempVin = 0;
+                    for (int i = 0; i < 4; i++) {
+                        if ((field[y+i][x+i]) == c) tempVin++;
+                        if (tempVin == vin) return true;
+                    }
+                }
+                if (isValidCell(x+3, y-3)){
+                    tempVin = 0;
+                    for (int i = 0; i < 4; i++) {
+                        if (field[y-i][x+i] == c) tempVin++;
+                        if (tempVin == vin) return true;
+                    }
+                }
             }
         }
         return false;
@@ -121,59 +124,78 @@ public class TicToe {
         return true;
     }
     private static void checkTurn(char c){
-        int Horizont, Vertical, Diag1, Diag2, Diag3, Diag4, Diag5, Diag6;
+        int tempX = 2;
+        int tempY = 2;
+        int tempVin;
+        int bestX = 2;
+        int bestY = 2;
         for (int y = 0; y < fieldSizeY; y++) {
-            Horizont = 0;
-            Vertical = 0;
-            Diag1 = 0;
-            Diag2 = 0;
-            Diag3 = 0;
-            Diag4 = 0;
-            Diag5 = 0;
-            Diag6 = 0;
             for (int x = 0; x < fieldSizeX; x++) {
-                if (field[y][x] == c) Horizont++;
-                else if ((Horizont>=3)&&(field[y][x]==DOT_EMPTY)){
-                    bestX = x;
-                    bestY = y;
+                if (isValidCell(x+3,y)){
+                    tempVin = 0;
+                    for (int i = 0; i < 4; i++) {
+                        if (field[y][x+i] == c) tempVin++;
+                        else if (field[y][x+i] == DOT_EMPTY){
+                            tempX = x + i;
+                            tempY = y;
+                        }
+                    }
+                    if ((tempVin==(vin - 1))&&(field[tempY][tempX]==DOT_EMPTY)){
+                        bestX = tempX;
+                        bestY = tempY;
+                    }
                 }
-                if (field[x][y] == c) Vertical++;
-                else if ((Vertical>=3)&&(field[y][x]==DOT_EMPTY)){
-                    bestX = x;
-                    bestY = y;
+                if (isValidCell(y, x + 3)) {
+                    tempVin = 0;
+                    for (int i = 0; i < 4; i++) {
+                        if (field[x+i][y] == c) tempVin++;
+                        else if (field[x+i][y] == DOT_EMPTY){
+                            tempX = y;
+                            tempY = x + i;
+                        }
+                    }
+                    if ((tempVin==(vin - 1))&&(field[tempY][tempX]==DOT_EMPTY)){
+                        bestX = tempX;
+                        bestY = tempY;
+                    }
                 }
-                if (field[x][x] == c) Diag1++;
-                else if ((Diag1>=3)&&(field[y][x]==DOT_EMPTY)){
-                    bestX = x;
-                    bestY = y;
+                if (isValidCell(x+3, y+3)){
+                    tempVin = 0;
+                    for (int i = 0; i < 4; i++) {
+                        if (field[y+i][x+i] == c) tempVin++;
+                        else if (field[y+i][x+i] == DOT_EMPTY){
+                            tempX = y + i;
+                            tempY = x + i;
+                        }
+                    }
+                    if ((tempVin==(vin - 1))&&(field[tempY][tempX]==DOT_EMPTY)){
+                        bestX = tempX;
+                        bestY = tempY;
+                    }
                 }
-                if (field[x][fieldSizeX - 1 - x] == c) Diag2++;
-                else if ((Diag2>=3)&&(field[y][x]==DOT_EMPTY)){
-                    bestX = x;
-                    bestY = y;
-                }
-                if ((x+1) < fieldSizeX && field[x][x+1] == c) Diag3++;
-                else if ((Diag3>=3)&&(field[y][x]==DOT_EMPTY)){
-                    bestX = x;
-                    bestY = y;
-                }
-                if (fieldSizeX-2-x > 0 && field[x][fieldSizeX-2-x] == c) Diag4++;
-                else if ((Diag4>=3)&&(field[y][x]==DOT_EMPTY)){
-                    bestX = x;
-                    bestY = y;
-                }
-                if ((x+1) < fieldSizeY && field[x+1][x] == c) Diag5++;
-                else if ((Diag5>=3)&&(field[y][x]==DOT_EMPTY)){
-                    bestX = x;
-                    bestY = y;
-                }
-                if (fieldSizeY-2-x > 0 && field[fieldSizeY-2-x][x] == c) Diag6++;
-                else if ((Diag6>=3)&&(field[y][x]==DOT_EMPTY)){
-                    bestX = x;
-                    bestY = y;
+                if (isValidCell(x+3, y-3)){
+                    tempVin = 0;
+                    for (int i = 0; i < 4; i++) {
+                        if (field[y-i][x+i] == c) tempVin++;
+                        else if (field[y-i][x+i] == DOT_EMPTY){
+                            tempX = x + i;
+                            tempY = y - i;
+                        }
+                    }
+                    if ((tempVin==(vin - 1))&&(field[tempY][tempX]==DOT_EMPTY)){
+                        bestX = tempX;
+                        bestY = tempY;
+                    }
                 }
             }
         }
+
+        if((tempX==2)&&(tempY==2)&&(c!=DOT_AI)){  //если лучшие варианты за игрока не найдены
+            checkTurn(DOT_AI);  // ищем лучшие варианты за компьютер
+            aiTurn(bestX,bestY);
+            return;
+        }
+        aiTurn(bestX,bestY);
     }
 
     public static void main(String[] args) {
@@ -190,20 +212,20 @@ public class TicToe {
                 System.out.println("Draw!");
                 break;
             }
-            vin--;//для проверки угрозы проигрыша/выигрыша
+            vin=3;//для проверки шанса выиграть
             if (checkWin(DOT_AI)){
+                vin = 4;
                 checkTurn(DOT_AI);
-                aiTurn(bestX,bestY);
             }
             else if (checkWin(DOT_HUMAN)){
+                vin = 4;
                 checkTurn(DOT_HUMAN);
-                aiTurn(bestX,bestY);
             }
             else {
                 aiTurn();
             }
+            vin = 4;
             printField();
-            vin++;//возвращаем условие выигрыша
             if (checkWin(DOT_AI)){
                 System.out.println("Computer win!");
                 break;
