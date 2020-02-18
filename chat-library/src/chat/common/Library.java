61f29d22b28b649d125f4370f37fd97148e43c6d
sergey-sv-git <andreev-sv@bk.ru>
2020-02-18 13:57:05 +0300

    корректировка

--- chat-library/src/chat/common/Library.java
+++ chat-library/src/chat/common/Library.java
@@ -7,6 +7,7 @@ public class Library {
     /auth_error
     /broadcast±msg
     /msg_format_error±msg
+    /user_list±user1±user2±user3±....
     * */
     public static final String DELIMITER = "±";
     public static final String AUTH_REQUEST = "/auth_request";
@@ -16,6 +17,16 @@ public class Library {
     // если мы вдруг не поняли, что за сообщение и не смогли разобрать
     public static final String TYPE_BROADCAST = "/bcast";
     // то есть сообщение, которое будет посылаться всем
+    public static final String TYPE_BCAST_CLIENT = "/client_msg";
+    public static final String USER_LIST = "/user_list";
+
+    public static String getTypeBcastClient(String msg) {
+        return TYPE_BCAST_CLIENT + DELIMITER + msg;
+    }
+
+    public static String getUserList(String users) {
+        return USER_LIST + DELIMITER + users;
+    }
 
     public static String getAuthRequest(String login, String password) {
         return AUTH_REQUEST + DELIMITER + login + DELIMITER + password;
